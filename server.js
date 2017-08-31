8c1fbff1781f9d1e06ae3cec59fe61e3771de092
Zachary Iles <zackiles@users.noreply.github.com>
2017-08-31 03:15:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ server.use(prerender.blacklist());
 // server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
-// server.use(prerender.inMemoryHtmlCache());
-// server.use(prerender.s3HtmlCache());
+server.use(require('prerender-redis-cache'));
 
 server.start();
